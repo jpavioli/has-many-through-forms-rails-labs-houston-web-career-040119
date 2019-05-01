a69f27335c9bd7e79738e7575ddb3cb69f8e4fae
jpavioli <justin.avioli@gmail.com>
2019-05-01 16:34:00 -0500

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ActiveRecord::Base
   has_many :comments
   has_many :posts, through: :comments
+
+  def user_attributes_username=(category_attributes_username)
+    user_attributes_username.values.each do |user_attribute|
+      user = User.find_or_create_by(user_attribute)
+      self.users << user
+    end
+  end
+
 end
